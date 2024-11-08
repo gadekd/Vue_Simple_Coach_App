35d050a64534ffea1e7dabf2f91380b37416e592
gadekd <danielwgadek@gmail.com>
2024-11-08 20:14:45 +0100

    sending a request to coach (new script files, new components)

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,13 +1,48 @@
 <template>
     <div>
-        REQUESTS
+        <base-card>
+            <header>
+                <h2>Requests Received</h2>
+            </header>
+            <ul v-if="hasRequests">
+                <request-item v-for="request in receivedRequests" :key="request.id" :email="request.userEmail" :message="request.message"></request-item>
+            </ul>
+            <h3 v-else>You haven't received any requests yet 😞</h3>
+        </base-card>
     </div>
 </template>
 
 <script>
+import RequestItem from '../../components/requests/RequestItem.vue';
 
+export default {
+    components: {
+        RequestItem
+    },
+    computed: {
+        receivedRequests() {
+            return this.$store.getters['requests/requests'];
+        },
+        hasRequests() {
+            return this.$store.getters['requests/hasRequests'];
+        }
+    }
+}
 </script>
 
 <style scoped>
+header {
+  text-align: center;
+}
 
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
 </style>
