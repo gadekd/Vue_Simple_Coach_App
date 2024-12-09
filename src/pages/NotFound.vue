654411b7b393f13153c69dfbbf3a96ce8a92beec
gadekd <danielwgadek@gmail.com>
2024-12-09 10:40:52 +0100

    "not found" page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        <h1>404 - not found</h1>
+        <base-card>
+          <h2>Page not found</h2>
+          <p>
+            This page could not be reached. Please check out list of all our
+            <router-link to="/coaches">coaches</router-link> to choose from.
+          </p>  
+        </base-card>
     </div>
 </template>
 
